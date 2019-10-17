1a4886ce805ff1fb55e234ebca16f7dda8fd6473
jasur-2902 <jshukurov@adelphi.edu>
2019-10-17 15:47:53 -0400

    structure design

---
+++ script.js
@@ -0,0 +1,19 @@
+// take in a single number and store it
+function takeInNumber(){
+  
+}
+
+// take in an oprator (+ - / *) for the operation
+function operator(){
+
+}
+
+// take the first stored value and the second number in the display field and perform the math operation
+function evaluate(){
+
+}
+
+// clear the display field
+function cleary(){
+
+}
